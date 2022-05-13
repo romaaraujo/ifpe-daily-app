04435bb4329ce61a5c898bdfe100db76aa407700
Alesson Alves <72222950+alesson998@users.noreply.github.com>
2022-05-13 00:51:33 -0300

    Update Analise de sentimentos.py

--- tests/Analise de sentimentos.py
+++ tests/Analise de sentimentos.py
@@ -23,6 +23,7 @@ df.Classificacao.value_counts()
 
 df.drop_duplicates(['Text'], inplace=True)
 
+
 posts = df["Text"]
 posts
 
@@ -34,4 +35,28 @@ nltk.download('stopwords')
 nltk.download('rslp')
 nltk.download('punkt')
 nltk.download('wordnet')
+
+
+Reviews_Rappi['content'] = Reviews_Rappi.apply(lambda row: nltk.word_tokenize(row['content']), axis=1) # Tokenização dos dados
+
+stopwords = stopwords.words(language)
+stopwords = list(set(stopwords))
+
+def remove_stopwords(words):
+    """Remover as Stopwords das palavras tokenizadas"""
+    new_words = []
+    for word in words:
+        if word not in stopwords:
+            new_words.append(word)
+    return new_words
+
+def to_lowercase(words):
+    """converter todos os caracteres para lowercase"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+
 
